0547aeb31e7896b8f1506190c74a4581c4ba6c17
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-08-11 14:13:21 +0800

    Update build_binary.sh

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -1,9 +1,9 @@
-binary="portainer"
-mkdir -p dist
+# binary="portainer"
+# mkdir -p dist
 
-cd 'api/cmd/portainer'
+# cd 'api/cmd/portainer'
 
-go get -t -d -v ./...
-GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
+# go get -t -d -v ./...
+# GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
 
-mv "${binary}" "../../../dist/portainer"
+# mv "${binary}" "../../../dist/portainer"
